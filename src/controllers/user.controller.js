49eeee593e9c3b714b15b206eea3ef099b0e7e24
nathanvr <jonathanvaro1004@gmail.com>
2022-06-02 00:35:49 -0500

    adds async await to user controller and review controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,66 +11,53 @@ module.exports = {
     }
   },
   //show ID
-  show(req, res) {
-    const { userId } = req.params;
-    User.findById(userId)
-      .select("-password")
-      .populate("bookings", "date")
-      .populate("reviews", "title message")
-      .populate("bookingsites", "title description")
-      .then((user) => {
-        res.status(200).json({ message: "User found", data: user });
-      })
-      .catch((err) => {
-        res.status(404).json({ message: "User not found", data: err });
-      });
+  async show(req, res) {
+    try {
+      const { userId } = req.params;
+      const user = await User.findById(userId)
+        .select("-password")
+        .populate("bookings", "date")
+        .populate("reviews", "title message")
+        .populate("bookingsites", "title description");
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(404).json(err);
+    }
   },
 
   //Create -POST
-  create(req, res) {
-    const data = req.body;
-    const newUser = {
-      ...data,
-    };
-    User.create(newUser)
-      .then((user) => {
-        res.status(201).json({ message: "user created", data: user });
-      })
-      .catch((err) => {
-        res
-          .status(400)
-          .json({ message: "user could not be created", data: err });
-      });
+  async create(req, res) {
+    try {
+      const data = req.body;
+      const newUser = { ...data };
+      const user = await User.create(newUser);
+      res.status(201).json({ message: "user created", data: user });
+    } catch (err) {
+      res.status(400).json({ message: "user could not be created", data: err });
+    }
   },
   //Update PUT
-  update(req, res) {
-    const { userId } = req.params;
-    User.findByIdAndUpdate(userId, req.body, {
-      new: true,
-      runValidators: true,
-      context: "query",
-    })
-      .then((user) => {
-        res.status(200).json({ message: "User updated", data: user });
-      })
-      .catch((err) => {
-        res
-          .status(400)
-          .json({ message: "User could not be updated", data: err });
+  async update(req, res) {
+    try {
+      const { userId } = req.params;
+      const user = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+        runValidators: true,
+        context: "query",
       });
+      res.status(200).json({ message: "User updated", data: user });
+    } catch (err) {
+      res.status(400).json({ message: "User could not be updated", data: err });
+    }
   },
   //Delete
-  destroy(req, res) {
-    const { userId } = req.params;
-
-    User.findByIdAndDelete(userId)
-      .then((user) => {
-        res.status(200).json({ message: "User deleted", data: user });
-      })
-      .catch((err) => {
-        res
-          .status(400)
-          .json({ message: "User could not be deleted", data: err });
-      });
+  async destroy(req, res) {
+    try {
+      const { userId } = req.params;
+      const user = await User.findByIdAndDelete(userId);
+      res.status(200).json({ message: "User deleted", data: user });
+    } catch (err) {
+      res.status(400).json({ message: "User could not be deleted", data: err });
+    }
   },
 };
