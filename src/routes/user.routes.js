8a15fc6ab44e13f890ff7526b1d795b1a3b6fd0c
nathanvr <jonathanvaro1004@gmail.com>
2022-06-03 11:24:18 -0500

    adds the auth, signup, signin

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/user.controller");
 router.route("/").get(userController.list);
 router.route("/:userId").get(userController.show);
 router.route("/").post(userController.create);
+router.route("/login").post(userController.signin);
 router.route("/:userId").put(userController.update);
 router.route("/:userId").delete(userController.destroy);
 
