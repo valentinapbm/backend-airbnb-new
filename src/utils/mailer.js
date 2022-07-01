2682a2aecf0c9b5c27145410e950b08adaef79a2
Valentina <valentina.paredes.11b@gmail.com>
2022-07-01 18:25:52 -0500

    add email

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,8 +6,8 @@ exports.transporter = nodemailer.createTransport({
   host: process.env.MAIL_SERVER,
   secure: false,
   auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_APPPASS,   
+        user: "airbnb.clon@gmail.com",
+        pass: "ycfxhijdsnpzyzmf"   
     },
   tls : { rejectUnauthorized: false}
 }); 
