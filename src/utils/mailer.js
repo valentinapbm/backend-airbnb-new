2ff40463bd367fefe02a5439b2aa189b13af3d53
Valentina <valentina.paredes.11b@gmail.com>
2022-07-07 14:14:32 -0500

    fina

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -211,7 +211,7 @@ exports.recoverypassword = (email, token, name) => {
                                   <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;width:300px;line-height:100%;">
                                     <tr>
                                       <td align="center" bgcolor="#5e6ebf" role="presentation" style="border:none;border-radius:3px;cursor:auto;mso-padding-alt:10px 25px;background:#5e6ebf;" valign="middle">
-                                        <a href="http://localhost:3000/password-recovery/${token}" style="display:inline-block;width:250px;background:#5e6ebf;color:#ffffff;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:17px;font-weight:bold;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:10px 25px;mso-padding-alt:0px;border-radius:3px;" target="_blank"> Recuperar contraseña </a>
+                                        <a href="https://airbnbclon.netlify.app/password-recovery/${token}" style="display:inline-block;width:250px;background:#5e6ebf;color:#ffffff;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:17px;font-weight:bold;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:10px 25px;mso-padding-alt:0px;border-radius:3px;" target="_blank"> Recuperar contraseña </a>
                                       </td>
                                     </tr>
                                   </table>
